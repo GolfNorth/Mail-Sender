a63aaf808a84386cc7782f82e4f44e801529761c
Vadim <golf155@yandex.ru>
2020-01-30 21:19:09 +0500

    Выполнены задания урока 1

---
+++ Test/WpfTestMailSender/SMTPServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfTestMailSender
+{
+    public static class SMTPServer
+    {
+        public static string Host { get; } = "smtp.yandex.ru";
+        public static string From { get; } = "webmaster@golfnorth.ru";
+        public static string To { get; } = "webmaster@golfnorth.ru";
+        public static int Port { get; } = 25;
+
+    }
+}
---
+++ Test/WpfTestMailSender/SendEndWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTestMailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для SendEndWindow.xaml
+    /// </summary>
+    public partial class SendEndWindow : Window
+    {
+        public SendEndWindow(string content, Window owner = null, Brush brush = null)
+        {
+            InitializeComponent();
+
+            lSendEnd.Content = content;
+
+            if (owner != null)
+                Owner = owner;
+
+            if (brush != null)
+                lSendEnd.Foreground = brush;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
