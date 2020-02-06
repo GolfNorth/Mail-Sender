5177f9fba0fd874ed51c84d10091c552b823d512
Vadim <golf155@yandex.ru>
2020-02-06 19:19:57 +0500

    Чистка кода Замена картинок и иконок Создание окна редактирования получателя Добавление команд

--- MailSender/ViewModels/DistributionGroupViewModel.cs
+++ MailSender/ViewModels/DistributionGroupViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using CommonServiceLocator;
 using MailSender.Enums;
 using MailSender.Library.Data;
 using MailSender.Library.Entities;
+using MailSender.Library.Services.Interfaces;
+using MailSender.Views;
 using Prism.Commands;
 using Prism.Mvvm;
 
@@ -10,23 +12,91 @@ namespace MailSender.ViewModels
 {
     public class DistributionGroupViewModel : BindableBase
     {
-        public DistributionGroupViewModel()
+        private readonly IRecipientsManager _recipientsManager;
+        private ObservableCollection<Recipient> _recipients;
+        private Recipient _selectedRecipient;
+        private ObservableCollection<Sender> _senders;
+        private ObservableCollection<Server> _servers;
+
+        public DistributionGroupViewModel(IRecipientsManager recipientsManager)
         {
+            Servers = new ObservableCollection<Server>(DevData.Servers);
+            Senders = new ObservableCollection<Sender>(DevData.Senders);
+
+            _recipientsManager = recipientsManager;
+
+            #region Реализация команд
+
             SwitchToScheduler = new DelegateCommand(() =>
             {
                 var mainWindowViewModel = ServiceLocator.Current.GetInstance<MainWindowViewModel>();
 
                 mainWindowViewModel.SelectedTabIndex = (int) MainWindowTabItems.Scheduler;
             });
+
+            LoadRecipientsDataCommand = new DelegateCommand(() =>
+            {
+                Recipients = new ObservableCollection<Recipient>(_recipientsManager.GetAll());
+            });
+
+            RecipientEditCommand = new DelegateCommand(() =>
+            {
+                var editWindow = new RecipientEditorWindow();
+
+                editWindow.ShowDialog();
+            }, () => SelectedRecipient != null).ObservesProperty(() => SelectedRecipient);
+
+            SaveRecipientChangesCommand = new DelegateCommand<Recipient>(recipient =>
+            {
+                _recipientsManager.Edit(recipient);
+                _recipientsManager.SaveChanges();
+            }, recipient => recipient != null).ObservesProperty(() => SelectedRecipient);
+
+            #endregion
         }
 
-        public List<Server> Servers => DevData.Servers;
-        public List<Sender> Senders => DevData.Senders;
-        public List<Recipient> Recipients => DevData.Recipients;
+        public ObservableCollection<Recipient> Recipients
+        {
+            get => _recipients;
+            private set => SetProperty(ref _recipients, value);
+        }
+
+        public ObservableCollection<Server> Servers
+        {
+            get => _servers;
+            set => SetProperty(ref _servers, value);
+        }
+
+        public ObservableCollection<Sender> Senders
+        {
+            get => _senders;
+            set => SetProperty(ref _senders, value);
+        }
+
+        public Recipient SelectedRecipient
+        {
+            get => _selectedRecipient;
+            set => SetProperty(ref _selectedRecipient, value);
+        }
 
         /// <summary>
         ///     Сменяет вкладку на "Планировщик"
         /// </summary>
         public DelegateCommand SwitchToScheduler { get; }
+
+        /// <summary>
+        ///     Загружает список получателей сообщений
+        /// </summary>
+        public DelegateCommand LoadRecipientsDataCommand { get; }
+
+        /// <summary>
+        ///     Открывает окно редактирование получателя
+        /// </summary>
+        public DelegateCommand RecipientEditCommand { get; }
+
+        /// <summary>
+        ///     Открывает окно редактирование получателя
+        /// </summary>
+        public DelegateCommand<Recipient> SaveRecipientChangesCommand { get; }
     }
 }
---
+++ MailSender/Views/RecipientEditorWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MailSender.Views
+{
+    /// <summary>
+    ///     Логика взаимодействия для RecipientEditorWindow.xaml
+    /// </summary>
+    public partial class RecipientEditorWindow : Window
+    {
+        public RecipientEditorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
