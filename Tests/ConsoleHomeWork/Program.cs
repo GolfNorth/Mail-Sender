919c897dd986c47bd956d024d12b4b4546a0b655
Vadim <golf155@yandex.ru>
2020-02-13 01:56:54 +0500

    Выполнено задания урока 5

--- Tests/ConsoleHomeWork/Program.cs
+++ Tests/ConsoleHomeWork/Program.cs
@@ -6,8 +6,8 @@
         {
             var menu = new Menu(args);
             menu.Add(new Task01());
-            menu.Add(new Task02());
-            menu.Print("Акчулпанов В.Г.\nУрок №5. Домашнее задание\nНаписать приложение, считающее в раздельных потоках:");
+            //menu.Add(new Task02());
+            menu.Print("Акчулпанов В.Г.\nУрок №5. Домашнее задание\n");
         }
     }
 }
--- Tests/ConsoleHomeWork/Task01.cs
+++ Tests/ConsoleHomeWork/Task01.cs
@@ -4,29 +4,72 @@ using System.Threading;
 /*
  * Написать приложение, считающее в раздельных потоках:
  * a. факториал числа N, которое вводится с клавиатуры;
+ * b. сумму целых чисел до N.
  */
 
 namespace ConsoleHomeWork
 {
     public class Task01 : ITask
     {
+        private Random _random;
+        private int poolCounter;
         private static readonly object __SyncRoot = new object();
 
-        public string Title { get; set; } = "сумму целых чисел до N.";
+        public string Title { get; set; } = "Написать приложение, считающее в раздельных потоках факториал числа N и сумму чисел до N";
 
         public void Run(string[] args)
         {
+            _random = new Random();
+            poolCounter = 0;
+
+            //var max = int.MaxValue;
+            var max = FindMaxValue();
+
+            //for (var i = max; i > 0; i--)
+            //{
+            //    var number = i;
+
+            //    poolCounter += 2;
+
+            //    ThreadPool.QueueUserWorkItem(o => Factorial(number));
+            //    ThreadPool.QueueUserWorkItem(o => CalcAmount(number));
+            //}
+
             while (true)
             {
                 int number;
                 lock (__SyncRoot)
-                    number = Common.ReadInt("Введите целое положительное число: ", 0, int.MaxValue);
+                    number = Common.ReadInt($"Введите число (от 0 до {max}): ", 0, max);
 
                 if (number == 0) break;
 
+                poolCounter += 2;
+
                 // number объявляется каждый раз внутри цикла, то нет смысла создавать копию
+                ThreadPool.QueueUserWorkItem(o => Factorial(number));
                 ThreadPool.QueueUserWorkItem(o => CalcAmount(number));
             }
+
+            while (poolCounter > 0)
+                Thread.Sleep(10);
+        }
+
+        public void Factorial(int n)
+        {
+            long sum = n == 0 ? 0 : 1;
+
+            for (var i = 1; i <= n; i++)
+            {
+                sum *= i;
+
+                // Задержка для наглядности
+                Thread.Sleep(_random.Next(100));
+            }
+
+            lock (__SyncRoot)
+                Console.WriteLine($"Факториал числа {n} равен {sum}");
+
+            poolCounter--;
         }
 
         public void CalcAmount(int n)
@@ -38,11 +81,33 @@ namespace ConsoleHomeWork
                 sum += i;
 
                 // Задержка для наглядности
-                Thread.Sleep(10);
+                Thread.Sleep(_random.Next(100));
             }
 
             lock (__SyncRoot)
-                Console.WriteLine($"Сумма положительных чисел до {n} равна {sum}");
+                Console.WriteLine($"Сумма чисел до {n} равна {sum}");
+
+            poolCounter--;
+        }
+
+        /// <summary>
+        /// Максимальное число для факториала на машине
+        /// </summary>
+        /// <returns></returns>
+        public int FindMaxValue()
+        {
+            var res = 2;
+            long fact = 2;
+
+            while (true)
+            {
+                if (fact < 0) break;
+
+                res++;
+                fact *= res;
+            }
+
+            return res - 1;
         }
     }
 }
