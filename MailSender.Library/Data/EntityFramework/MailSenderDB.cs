a7aa1fcb40950e5ce847466e2b956899f12f06f3
Vadim <golf155@yandex.ru>
2020-02-19 16:45:28 +0500

    Добавлен контекст БД

---
+++ MailSender.Library/Data/EntityFramework/MailSenderDB.cs
@@ -0,0 +1,19 @@
+﻿using MailSender.Library.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MailSender.Library.Data.EntityFramework
+{
+    public class MailSenderDB : DbContext
+    {
+        public MailSenderDB(DbContextOptions opt) : base(opt)
+        {
+        }
+
+        public DbSet<Email> Emails { get; set; }
+        public DbSet<EmailList> EmailLists { get; set; }
+        public DbSet<Recipient> Recipients { get; set; }
+        public DbSet<SchedulerTask> SchedulerTasks { get; set; }
+        public DbSet<Sender> Senders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+    }
+}
